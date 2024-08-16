9457687c5df80d964635dff5a8b7be10d4474fd4
borsu1 <borsudaniel@gmail.com>
2024-08-16 12:40:49 -0700

    updating output code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -17,17 +17,24 @@ def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'Mozilla/5.0'}
 
-    response = requests.get(
-        url, headers=headers, allow_redirects=False
-    )
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as http_err:
+        print("None")
+        return
+    except Exception as err:
+        print("None")
+        return
 
     if response.status_code == 200:
         data = response.json()
         posts = data['data']['children']
         for post in posts:
             print(post['data']['title'])
+        print("OK")
     else:
-        print(None)
+        print("None")
 
 
 # Example usage
