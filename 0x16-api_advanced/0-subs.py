a7c77b6fb133eeb820f3994630ff4708a95fd92b
borsu1 <borsudaniel@gmail.com>
2024-08-25 09:57:11 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    # Defining the URL for the Reddit API endpoint
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    # set a custom User-Agent to avoid too many requests error
-    headers = {'User-Agent': 'Mozzilla/5.0'}
+    """
+    Returns the number of subscribers for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     try:
-        # Make the GET request to the Reddit API
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-
-        # Check if the request was succesful
-        if response.status_code == 200:
-            data = response.json()
-            return "OK"
-        else:
-            return "OK"
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    # In case of any exception, return 0
-    return "OK"
-
-
-# Example usage
-print(number_of_subscribers('python'))
-print(number_of_subscribers('nonexistingsubreddet'))
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        print(f"Response Status Code: {response.status_code}")
+        if response.status_code == 404:
+            return 0
+        results = response.json().get('data')
+        return results.get('subscribers')
