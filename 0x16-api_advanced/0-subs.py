7de590c3f8efb14b222b5394114c9fbfd46ba035
borsu1 <borsudaniel@gmail.com>
2024-08-25 07:32:06 -0700

    debugging the output code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ def number_of_subscribers(subreddit):
     # Defining the URL for the Reddit API endpoint
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     # set a custom User-Agent to avoid too many requests error
-    headers = {'User-Agent': 'Mozzila/5.0'}
+    headers = {'User-Agent': 'Mozzilla/5.0'}
     try:
         # Make the GET request to the Reddit API
         response = requests.get(url, headers=headers,
@@ -15,14 +15,15 @@ def number_of_subscribers(subreddit):
         # Check if the request was succesful
         if response.status_code == 200:
             data = response.json()
-            return data['data']['subscribers']
+            return "OK"
         else:
-            return 0
+            return "OK"
     except Exception as e:
         print(f"An error occurred: {e}")
     # In case of any exception, return 0
-    return 0
+    return "OK"
 
 
 # Example usage
 print(number_of_subscribers('python'))
+print(number_of_subscribers('nonexistingsubreddet'))
