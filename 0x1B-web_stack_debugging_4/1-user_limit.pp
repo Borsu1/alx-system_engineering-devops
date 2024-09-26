1da419766667d2372f765025ed2ffca0229cafad
Borsu1 <borsudaniel@gmail.com>
2024-09-26 03:26:58 +0000

    Updated the systctl command path

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,20 @@
-# Reconfigure the OS for 'holberton' to login and open a file without any error message
+# Reconfigure the OS for 'holberton' to login and open a file without any error messages
 
 exec { 'increase-hard-file-limit-holberton-user':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  command => '/bin/echo "holberton hard nofile 4096" >> /etc/security/limits.conf',
+  unless  => '/bin/grep -q "holberton hard nofile 4096" /etc/security/limits.conf',
+  path    => ['/bin', '/usr/bin'],
 }
 
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'increase-soft-file-limit-holberton-user':
+  command => '/bin/echo "holberton soft nofile 2048" >> /etc/security/limits.conf',
+  unless  => '/bin/grep -q "holberton soft nofile 2048" /etc/security/limits.conf',
+  path    => ['/bin', '/usr/bin'],
+}
+
+exec { 'apply-limits':
+  command     => '/sbin/sysctl -p',
+  refreshonly => true,
+  subscribe   => [Exec['increase-hard-file-limit-holberton-user'], Exec['increase-soft-file-limit-holberton-user']],
+  path        => ['/sbin', '/bin', '/usr/bin'],
 }
