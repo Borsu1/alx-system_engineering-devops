335df2356de13de725b63d80acad61a3c15b9dd7
borsu1 <borsudaniel@gmail.com>
2024-09-27 16:21:39 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,17 +4,28 @@ Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
+import logging
 
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    Return the number of subscribers on a given Reddit subreddit.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
         data = response.json()
         subscribers = data['data']['subscribers']
         print("OK")
         return subscribers
-    else:print("OK")
-        return 0
+    except requests.exceptions.HTTPError as http_err:
+        logging.error(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        logging.error(f"An error occurred: {err}")
+    print("OK")
+    return 0
